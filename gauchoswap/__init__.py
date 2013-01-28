89e4e527cd28ba171437c61b58af055d41423349
Olaoluwa Osuntokun <laolu32@gmail.com>
2013-01-27 16:14:56 -0800

    Updated app init code

--- gauchoswap/__init__.py
+++ gauchoswap/__init__.py
@@ -1,25 +1,33 @@
 import os
 
-from flask import Flask, render_template, g, session
+from flask import Flask, render_template
+from flask_oauth import OAuth
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from views import frontend
 
+SECRET_KEY = '2432fkdsajflads9'
+DEBUG = True
+FACEBOOK_APP_ID = '542774122412862'
+FACEBOOK_APP_SECRET = '8125ade96b956dc61982de537d7a6389'
 
-app = Flask('gauchospace')
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.debug = os.environ.get('DEBUG')
+app.debug = True
+app.secret_key = SECRET_KEY
 
 db = SQLAlchemy(app)
+oauth = OAuth()
 
 
+'''
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-
+'''
+from gauchoswap.views import frontend
 app.register_blueprint(frontend.mod)
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
