000c632c09ec49b7256b978ea39e16bc5f24ea0d
Aaron Magat <AaronMGT@gmail.com>
2013-02-10 17:19:18 -0800

    testing ability to commit

--- gauchoswap/views/swapblock.py
+++ gauchoswap/views/swapblock.py
@@ -3,9 +3,10 @@ from flask import (redirect, url_for, session, request, Blueprint, render_templa
 from gauchoswap import db, oauth, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET
 from gauchoswap.models import Student
 
-
-mod = Blueprint('swapblock', __name__)
+mod = Blueprint('swapblock', __name__, url_prefix='/swapblock')
 
 @mod.route('/swapblock/')
 def get():
-   return redirect(url_for('frontend.swap_block')) 
+   user = Student.query.filter_by(facebook_id=session['fb_id']).first()
+   username = user.name
+   return redirect(url_for('frontend.swap_block', username=username)) 
