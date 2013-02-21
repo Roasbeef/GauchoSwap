4047137b58aa80cee9a7bf2b7f0cd329e6add844
JApusen <jay@ubuntu.(none)>
2013-02-20 18:09:05 -0800

    lab.py

--- gauchoswap/api.py
+++ gauchoswap/api.py
@@ -7,7 +7,7 @@ def get_or_404(f):
     def wrapper(*args, **kwargs):
         result = f(*args, **kwargs)
         if result is None:
-            raise DbLectureError('Model requested does not exist')
+            raise DbNotFoundError('Model requested does not exist')
         else:
             return result
     return wrapper
@@ -21,3 +21,21 @@ class DbLectureError(Exception):
 def get_all_lectures(json=False):
     all_lectures = Lecture.query.all()
     return (lecture.to_json() for lecture in all_lectures) if json else (lecture for lecture in all_lectures)
+
+@get_or_404
+def get_all_labs(json=False):
+    all_labs = Lab.query.all()
+    return (lab.to_json() for lab in all_labs) if json else (lab for lab in all_labs)
+
+@get_or_404
+def get_lab_by_department(department, json=''):
+    labs = Lab.query.filter_by(department = department).all()
+    return (lab.to_json() for lab in labs) if json else (lab for lab in labs)
+
+@get_or_404
+def get_lab_by_id(lab_id, json=''):
+    lab = Lab.query.filter_by(lab_id = lab_id)
+    return lab.to-json() if json else lab
+
+
+    
--- gauchoswap/views/lab.py
+++ gauchoswap/views/lab.py
@@ -1,3 +1,31 @@
 from flask import Blueprint
-
+from gauchoswap.helpers import request_wants_json
 mod = Blueprint('lab', __name__, url_prefix='/lab')
+
+def get_all_labs():
+    try:
+        labs = api.get_all_labs()
+    except api.DbNotFoundError:
+        abort(404)
+        
+    return jsonify(labs)
+
+@mod.route('/<department>/', methods=['GET'])
+def get_lab_by_department(department):
+    wants_json = request_wants_json();
+    try:
+        labs = api.get_lab_by_department(department, json=wants_json)
+    except api.DbNotFoundError:
+        abort(404)
+
+    return jsonify(labs)
+
+@mod.route('/<int:lab_id>/', methonds=['GET'])
+def get_lab_by_id(lab_id):
+     wants_json = request_wants_json();
+    try:
+        lab = api.get_lab_by_id(lab_id, json=wants_json)
+    except api.DbNotFoundError:
+        abort(404)
+
+    return jsonify(lab)
