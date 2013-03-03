d4a53251b12327c83fa6257dcc1e5a464747c773
Olaoluwa Osuntokun <laolu32@gmail.com>
2013-03-02 16:15:33 -0800

    Start of SwapBlock UI, will finish maybe later today

--- gauchoswap/views/student.py
+++ gauchoswap/views/student.py
@@ -9,8 +9,21 @@ mod = Blueprint('student', __name__, url_prefix='/student')
 @mod.route('/<int:student_id>')
 def user_profile(student_id):
     student = Student.query.filter_by(id=student_id).first_or_404()
+    swapblock = student.swapblock.first()
+
+    owned_classes = []
+    wanted_classes = []
+
+    owned_classes.extend(swapblock.owned_lectures)
+    owned_classes.extend(swapblock.owned_sections)
+    owned_classes.extend(swapblock.owned_labs)
+
+    wanted_classes.extend(swapblock.wanted_lectures)
+    wanted_classes.extend(swapblock.wanted_sections)
+    wanted_classes.extend(swapblock.wanted_labs)
 
     departments = [(abrv, department) for abrv, department in course_abrev.course_abrv_to_department.iteritems()]
     departments.sort()
 
-    return render_template('student.html', student=student, departments=departments)
+    return render_template('student.html', student=student, departments=departments,
+                           owned_classes=owned_classes, wanted_classes=wanted_classes)
